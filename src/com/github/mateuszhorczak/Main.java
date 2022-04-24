b51456f18ab32900a514b3b2566d15e65b4f7bfe
mhorczak <82621@student.pb.edu.pl>
2022-04-24 22:37:54 +0100

    plan maina

--- src/com/github/mateuszhorczak/Main.java
+++ src/com/github/mateuszhorczak/Main.java
@@ -3,12 +3,31 @@ package com.github.mateuszhorczak;
 public class Main {
 
     public static void main(String[] args) {
-        Projekt w grupie na zajecia z programowania
+        while(true) {
+            System.out.println("W jakim miejscu chcesz zrealizowac platnosc");
+            //wyswietl
+            //sklep, firma, cos 1 - 2 - 3 switche
+            //
+            //Wybierz rodzaj karty
+            //podaj numer karty, (opcjonalnie)imie i nazwisko
+            //wyswietlaja sie uslugi i cennim
+            //podaj usluge
+            //sprawdza czy masz kase
+            //jesli masz to potwierdza platnosc, jesli nie to odrzuca
 
-        Centrum obsługi kart płatniczych obsługuje płatności kartami kredytowymi, debetowymi i bankomatowymi. Z żądaniem autoryzacji każdej płatności mogą zwracać się sklepy, zakłady usługowe i firmy transportowe (to są klienci centrum). Centrum autoryzuje płatność po wysłaniu zapytania do banku który wydał daną kartę płatniczą a informację o tym archiwizuje w postaci wpisu w swoim systemie bazodanowym, które można przeszukiwać.
 
-        Uwaga: klienci firm są też klientami banku i tam mają konta - nie muszą być rejestrowani w firmach, płatność odbywa się (jak w rzeczywistości) podając dane karty i kwotę. Są 2 relacje klient: firma jest klientem centrum, osoba jest klientem firmy i zarazem klientem banku, w którym ma konto.
 
-                Minimalny zakres funkcjonalności: zarządzenie firmami korzystającymi z centrum centrum i bankami (dodawanie, usuwanie, przegląd) zarządzanie kartami (przypisana do klienta banku, wydana przez bank) obsługa żądań autoryzacji (kwota, data, dane karty) - decyzja zależna od rodzaju karty zapis i odczyt stanu systemu na dysk (realizacja archiwum operacji w postaci odrębnego pliku) przeszukiwanie archiwum za pomocą złożonych warunków (firma, bank, numer karty, właściciel, kwota, warunki OR i AND)
+            //Tryb serwisanta
+            //Zaloz konto w banku
+            //Podaj imie, nazwisko, kwote jaka chcesz wplacic na konto
+            //podaj rodzaj karty
+            //dostajesz wygenerowany numer konta z przypisana kwota
+
+            //usun konto w banku
+            //usuwa karte z kolekcji o danym numerze
+
+            //dodaje wpis z transkakcji do archwium
+        }
+
     }
 }
