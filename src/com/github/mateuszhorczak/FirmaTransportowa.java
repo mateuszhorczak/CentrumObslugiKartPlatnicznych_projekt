7ea55a33628f389affaa284e01747f2796d788ee
mhorczak <82621@student.pb.edu.pl>
2022-04-26 18:38:30 +0100

    wywalone uslugi double

--- src/com/github/mateuszhorczak/FirmaTransportowa.java
+++ src/com/github/mateuszhorczak/FirmaTransportowa.java
@@ -2,11 +2,7 @@ package com.github.mateuszhorczak;
 
 public class FirmaTransportowa {
     private String nazwa;
-    //
-    //nie mam przekonania do tego cennika, trzeba sprobowac to inaczej rozgryzc
-    //
-    private double przewoz = 299.99;
-    private double dowodz = 19.99;
+
 
     public FirmaTransportowa(String nazwa) {
         this.nazwa = nazwa;
