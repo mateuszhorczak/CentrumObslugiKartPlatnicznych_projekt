e1b30d4610989d88e13e8b12b97667a5b1abb6f5
mhorczak <82621@student.pb.edu.pl>
2022-05-22 17:18:09 +0100

    od nowa blac

--- src/com/github/mateuszhorczak/Sklep.java
+++
@@ -1,9 +0,0 @@
-package com.github.mateuszhorczak;
-
-public class Sklep {
-    private String nazwa;
-
-    public Sklep(String nazwa) {
-        this.nazwa = nazwa;
-    }
-}
--- src/com/github/mateuszhorczak/ZakladUslugowy.java
+++
@@ -1,9 +0,0 @@
-package com.github.mateuszhorczak;
-
-public class ZakladUslugowy {
-    private String nazwa;
-
-    public ZakladUslugowy(String nazwa) {
-        this.nazwa = nazwa;
-    }
-}
